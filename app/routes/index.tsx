1bfd3d6935784e1ab0c1584333fdd234cfc14c61
Helmi Satria <satriahelmi@gmail.com>
2022-09-02 22:32:02 +0700

    feat: completing footer

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,6 +8,25 @@ import { NatanyaSvg } from "../svg/Natanya";
 import { NatatokoSvg } from "../svg/Natatoko";
 
 const CONTENT = {
+  writings: [
+    {
+      url: "https://medium.com/@helmisatria/cara-mendapatkan-data-tweet-dari-twitter-e0ce79cdeed4",
+      text: "Cara mendapatkan data tweet dari Twitter",
+    },
+    {
+      url: "https://medium.com/@helmisatria/line-bot-101-cara-mudah-membuat-bot-di-line-61cd488f1b42",
+      text: "Cara Mudah Membuat Bot di Line",
+    },
+  ],
+  technologiesOftenUsed: [
+    { img: IconGovtech, name: "Typescript" },
+    { img: IconGovtech, name: "Remix.run" },
+    { img: IconGovtech, name: "Next.js" },
+    { img: IconGovtech, name: "Redis" },
+    { img: IconGovtech, name: "Docker" },
+    { img: IconGovtech, name: "PostgreSQL" },
+    { img: IconGovtech, name: "Cloudflare Worker" },
+  ],
   projects: [
     {
       icon: NatanyaSvg,
@@ -20,12 +39,19 @@ const CONTENT = {
           <a
             className="underline"
             href="https://github.com/helmisatria/natanya"
+            target="_blank"
+            rel="noreferrer"
           >
             https://github.com/helmisatria/natanya
           </a>
           <br />
           Live project hosted in vercel:{" "}
-          <a className="underline" href="https://natanya.vercel.app">
+          <a
+            className="underline"
+            href="https://natanya.vercel.app"
+            target="_blank"
+            rel="noreferrer"
+          >
             https://natanya.vercel.app
           </a>
         </p>
@@ -63,7 +89,12 @@ const CONTENT = {
       lowerDescription: () => (
         <p className="!leading-[180%] text-sm md:text-base">
           Check the application here:{" "}
-          <a href="https://natatoko.com" className="underline">
+          <a
+            href="https://natatoko.com"
+            className="underline"
+            target="_blank"
+            rel="noreferrer"
+          >
             https://natatoko.com
           </a>
         </p>
@@ -280,7 +311,12 @@ export default function Index() {
                           className="text-sm !leading-[180%] font-semibold text-gray-600 shadow-sm border-1 border-gray-100 rounded-lg bg-white"
                         >
                           <div className="flex flex-col items-center space-y-3 py-2 px-3">
-                            <img src={technology.img} alt="" className="h-10" />
+                            <img
+                              loading="lazy"
+                              src={technology.img}
+                              alt={technology.name}
+                              className="h-10"
+                            />
                             <span>{technology.name}</span>
                           </div>
                         </li>
@@ -350,13 +386,14 @@ export default function Index() {
                       Preview
                     </h4>
 
-                    <ul className="flex space-x-3 mt-5">
+                    <ul className="flex flex-col md:flex-row md:space-x-3 space-y-3 mt-5">
                       {project.demo.map((demo, index) => (
                         <li key={index}>
                           <img
+                            loading="lazy"
                             width={200}
                             height={120}
-                            className="w-[12.5rem] h-[7.5rem] object-cover rounded-lg shadow border border-sky-50"
+                            className="md:w-[12.5rem] md:h-[7.5rem] w-full h-[8rem] object-cover rounded-lg shadow border border-sky-50"
                             src={demo.img}
                             alt={demo.alt}
                           />
@@ -370,6 +407,118 @@ export default function Index() {
           </div>
         </div>
       </section>
+
+      <section className="bg-sky-50 bg-opacity-60">
+        <div className="max-w-5xl mx-auto px-6 md:pb-24 py-16">
+          <h2 className="md:mb-16 mb-10 text-2xl md:text-[2rem] font-extrabold -tracking-[0.03em] text-slate-900">
+            <span className="inline-block mr-2">⚙️</span> Technologies Often
+            Used
+          </h2>
+
+          <ul className="flex gap-3 md:max-w-[32rem] flex-wrap">
+            {CONTENT.technologiesOftenUsed.map((technology, index) => (
+              <li
+                key={index}
+                className="text-sm md:text-base !leading-[180%] font-semibold shadow-sm border-1 border-gray-100 rounded-lg bg-white"
+              >
+                <div className="flex flex-col items-start space-y-3 py-2 px-3">
+                  <img
+                    loading="lazy"
+                    src={technology.img}
+                    alt={technology.name}
+                    className="h-10"
+                  />
+                  <span>{technology.name}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      <section className="bg-sky-50 bg-opacity-60 pb-16">
+        <div className="max-w-5xl mx-auto px-6 md:pb-24 py-16">
+          <h2 className="md:mb-16 mb-10 text-2xl md:text-[2rem] font-extrabold -tracking-[0.03em] text-slate-900">
+            <span className="inline-block mr-2">✍️</span> Writings
+          </h2>
+
+          <p className="!leading-[180%] md:max-w-[37rem] mb-8 text-sm md:text-base">
+            It is not a big list, but currently I'm in the way to add more
+            writings while actively updating my youtube channel
+          </p>
+
+          <ul className="!leading-[180%] flex gap-3 md:max-w-[32rem] flex-wrap list-disc">
+            {CONTENT.writings.map((writing, index) => (
+              <li
+                key={index}
+                className="pl-2 ml-4 text-sky-800 md:text-lg hover:underline"
+              >
+                <a
+                  href={writing.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="visited:text-sky-900"
+                >
+                  {writing.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      <footer className="bg-sky-900 pb-8 md:pb-[5rem] md:pt-[9rem] pt-[4rem]">
+        <div className="max-w-5xl px-6 mx-auto text-white">
+          <h2 className="font-bold text-2xl md:text-3xl">
+            🤝 Let's get in touch
+          </h2>
+
+          <p className="text-sky-200 flex flex-col-reverse md:flex-col font-semibold leading-[150%] md:leading-[130%] text-3xl md:text-[4rem] pt-[5rem] md:pb-[4.5rem] pb-10">
+            <span className="inline">Suka kopi, mau ngopi bareng?</span>
+            <span className="inline">Have any project ideas?</span>
+            <span className="inline">Ask something?</span>
+          </p>
+
+          <p className="text-sky-50 text-sm md:text-xl md:mb-[9rem] mb-20">
+            Contact me via{" "}
+            <a
+              className="underline"
+              target="_blank"
+              href="mailto:satriahelmi@gmail.com"
+              rel="noreferrer"
+            >
+              email
+            </a>
+            ,{" "}
+            <a
+              className="underline"
+              target="_blank"
+              href="https://twitter.com/helmisatria_"
+              rel="noreferrer"
+            >
+              twitter
+            </a>
+            , or{" "}
+            <a
+              className="underline"
+              target="_blank"
+              href="https://www.youtube.com/channel/UC5ZR_JynIx14bmcZP_Pz8vw"
+              rel="noreferrer"
+            >
+              my youtube channel
+            </a>{" "}
+            <span className="text-sky-300">(boleh subscribe 😜)</span>
+          </p>
+
+          <p className="font-semibold text-sky-300 text-opacity-70 text-sm md:w-full">
+            Made with{" "}
+            <a href="https://remix.run" target="_blank" rel="noreferrer">
+              Remix.run
+            </a>{" "}
+            and designed with love ❤️ by Helmi Satria
+          </p>
+        </div>
+      </footer>
     </main>
   );
 }
