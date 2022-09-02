cf69e56d1a9b7f60014f46483f50825781ef3b83
Helmi Satria <satriahelmi@gmail.com>
2022-09-03 00:08:48 +0700

    chore: update meta

--- app/seo.ts
+++ app/seo.ts
@@ -1,20 +1,22 @@
 import { initSeo } from "remix-seo";
 
 export const { getSeo, getSeoMeta, getSeoLinks } = initSeo({
-  title: "Helmi Satria - Fullstack Javascript Developer",
+  title: "Helmi Satria - Full-stack Javascript Developer",
   description:
-    "Experienced fullstack javascript developer with 4+ years of experience in building web applications. Highly interested in building huge userbase applications using any web technology",
+    "Experienced full-stack javascript developer with 4+ years of experience in building web applications. Highly interested in building huge userbase applications using any web technology",
   twitter: {
     image: {
-      url: "https://res.cloudinary.com/helmisatria/image/upload/c_fill,h_500,w_500/v1662137627/Helmi_Satria_compressed.jpg",
+      url: "https://res.cloudinary.com/helmisatria/image/upload/c_fill,h_400,w_400/v1662137627/Helmi_Satria_compressed.jpg",
       alt: "Helmi Satria",
     },
   },
   openGraph: {
     images: [
       {
+        height: 400,
+        width: 400,
         alt: "Helmi Satria",
-        url: "https://res.cloudinary.com/helmisatria/image/upload/c_fill,h_500,w_500/v1662137627/Helmi_Satria_compressed.jpg",
+        url: "https://res.cloudinary.com/helmisatria/image/upload/c_fill,h_400,w_400/v1662137627/Helmi_Satria_compressed.jpg",
       },
     ],
   },
