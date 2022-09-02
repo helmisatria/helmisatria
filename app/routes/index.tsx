86c69efdfe56bb99635d5d284b9e40bc2af22800
Helmi Satria <satriahelmi@gmail.com>
2022-09-02 23:28:22 +0700

    style: fix animation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -191,8 +191,6 @@ export default function Index() {
       >
         <div className="flex flex-col md:flex-row items-center md:items-start max-w-5xl md:space-x-10 mx-auto">
           <img
-            data-sal="zoom-in"
-            data-sal-duration="500"
             height={160}
             width={160}
             className="w-40 h-40 md:w-[180px] md:h-[180px]"
@@ -200,29 +198,17 @@ export default function Index() {
             alt="helmi satria"
           />
 
-          <div
-            data-sal-easing="ease-out-back"
-            className="flex flex-col items-center md:items-start md:max-w-[29rem]"
-          >
-            <div
-              data-sal="slide-up"
-              className="text-xs py-1 mt-7 md:mt-0 px-3 rounded-xl bg-sky-600 text-white font-bold"
-            >
+          <div className="flex flex-col items-center md:items-start md:max-w-[29rem]">
+            <div className="text-xs py-1 mt-7 md:mt-0 px-3 rounded-xl bg-sky-600 text-white font-bold">
               <span>Fullstack Javascript Developer</span>
             </div>
 
-            <h1
-              data-sal="slide-up"
-              className="text-3xl sm:text-5xl my-4 md:mt-4 md:mb-5 font-extrabold text-sky-900 -tracking-[0.03em]"
-            >
+            <h1 className="text-3xl sm:text-5xl my-4 md:mt-4 md:mb-5 font-extrabold text-sky-900 -tracking-[0.03em]">
               Helmi Satria{" "}
               <span className="text-[2.5rem] md:text-5xl ">👋</span>
             </h1>
 
-            <p
-              data-sal="slide-up"
-              className="text-sm sm:text-base !leading-[160%] text-center md:text-left font-medium text-sky-900"
-            >
+            <p className="text-sm sm:text-base !leading-[160%] text-center md:text-left font-medium text-sky-900">
               Fullstack Javascript Developer who actively shares things on my
               Youtube and Twitter accounts about web development and other
               things to help other engineers in Indonesia
@@ -230,11 +216,7 @@ export default function Index() {
           </div>
         </div>
 
-        <p
-          data-sal="slide-up"
-          data-sal-delay="200"
-          className="max-w-5xl text-sky-900 mx-auto text-xl md:text-3xl !leading-[180%] py-14 md:pb-20 border-b-2 border-sky-100"
-        >
+        <p className="max-w-5xl text-sky-900 mx-auto text-xl md:text-3xl !leading-[180%] py-14 md:pb-20 border-b-2 border-sky-100">
           Hi, I am a software engineer with more than 4 yoe in using Vue.js and
           5 yoe in using Node.js for full-stack development. I'm enthusiastic to
           explore new things and highly interested in building huge userbase
