3a2d27fb9b89e24d030581e356b337c19b80f953
Helmi Satria <satriahelmi@gmail.com>
2022-09-03 09:05:39 +0700

    fix: try new og

--- app/seo.ts
+++ app/seo.ts
@@ -4,11 +4,17 @@ export const { getSeo, getSeoMeta, getSeoLinks } = initSeo({
   title: "Helmi Satria - Full-stack Javascript Developer",
   description:
     "Experienced full-stack javascript developer with 4+ years of experience in building web applications. Highly interested in building huge userbase applications using any web technology",
+  twitter: {
+    image: {
+      url: "https://res.cloudinary.com/helmisatria/image/upload/v1662170636/helmisatria.com/v2/Helmi%20Satria%20Twitter.png",
+      alt: "Helmi Satria",
+    },
+  },
   openGraph: {
     images: [
       {
-        height: 400,
-        width: 400,
+        height: 300,
+        width: 300,
         alt: "Helmi Satria",
         url: "https://res.cloudinary.com/helmisatria/image/upload/c_fill,h_400,w_400/v1662137627/Helmi_Satria_compressed.jpg",
       },
