aa688cb4b6ee21a54e47e3ae490c9c80a2419f31
Helmi Satria <satriahelmi@gmail.com>
2022-09-03 08:44:25 +0700

    fix: remove card twitter meta

--- app/seo.ts
+++ app/seo.ts
@@ -4,12 +4,6 @@ export const { getSeo, getSeoMeta, getSeoLinks } = initSeo({
   title: "Helmi Satria - Full-stack Javascript Developer",
   description:
     "Experienced full-stack javascript developer with 4+ years of experience in building web applications. Highly interested in building huge userbase applications using any web technology",
-  twitter: {
-    image: {
-      url: "https://res.cloudinary.com/helmisatria/image/upload/c_fill,h_400,w_400/v1662137627/Helmi_Satria_compressed.jpg",
-      alt: "Helmi Satria",
-    },
-  },
   openGraph: {
     images: [
       {
