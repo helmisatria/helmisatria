fc67fb0e177d7e7a3106bedef94877dc9913e26d
Helmi Satria <satriahelmi@gmail.com>
2022-12-23 19:49:28 +0700

    fix: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), partytown()]
+  integrations: [tailwind(), partytown()],
+  output: "server",
+  adapter: cloudflare(),
 });
