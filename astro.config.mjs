3a554e2cceabec64f86b5fe3a2092edabc7e8778
Helmi Satria <satriahelmi@gmail.com>
2022-12-23 19:40:48 +0700

    feat: init project with some integrations

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), partytown()]
+});
