11af259311db9d8fe0ef113ef8792decf539203d
Helmi Satria <satriahelmi@gmail.com>
2022-12-29 00:02:24 +0700

    V2 (#2)
    * feat: init project with some integrations
    
    * fix: add cloudflare adapter
    
    * chore: add nvmrc
    
    * chore: node 16
    
    * fix: replace google font with local font
    
    * feat: slicing homepage
    
    * style: more advanced styling for all breakpoints
    
    * fix: padding styling
    
    * style: move to react display on lg
    
    * style: finalize spacing
    
    * feat: add GA
    
    * fix: GA push
    
    * fix: GA
    
    * fix: trying to resolve partytown issue
    
    * fix: GA
    
    * fix: try use GA without partytown
    
    * fix: try use GA without partytown
    
    * fix: GA with previous id
    
    * feat: finish section hero slicing
    
    * fix: ournament become image instead of svg
    
    * chore: add favicon
    
    * style: fix gradient and spacing
    
    * fix: style bugs on some breakpoints
    
    * feat: og image
    
    * feat: og image
    
    * fix: og image size
    
    * fix: og image size
    
    * fix: og
    
    * fix: og images
    
    * fix: og again
    
    * fix: favicon
    
    * fix: new favicon
    
    * feat: init slicing section projects
    
    * style: fix z index and margin
    
    * feat: add slider and github links
    
    * feat: responsive projects
    
    * fix: responsive tech in project
    
    * fix: perf issue on mobile screen
    
    * fix: img aspect ratio
    
    * fix: img aspect ratio
    
    * fix: hide img on mobile
    
    * fix: img aspect ratio
    
    * fix: img aspect ratio
    
    * feat: init work experiences section
    
    * feat: responsive work experience header
    
    * feat: responsive work experience content
    
    * feat: add some gradient and mesh
    
    * fix: no pointer events for img
    
    * feat: section writing
    
    * style: adjust spacing and some size on small laptops
    
    * fix: responsive style on hero section
    
    * fix: optimize reading on hero
    
    * fix: lazy load img
    
    * fix: title highlight
    
    * style: positioning highlights
    
    * feat: responsive contact section
    
    * fix: aspect ratio
    
    * fix: rm duplicate aspect-auto
    
    * fix: contact body content font size
    
    * style: letter spacing contact title
    
    * feat: add buka on work experience
    
    * feat: add natatoko as project
    
    * feat: update content on project natatokog
    
    * style: fix some spacings and header shine positioning
    
    * feat: add more accent color to fit the content
    
    * fix: aspect ratio
    
    * fix: ournament on work exp
    
    * fix: missing ournament on work exp section
    
    * feat: add project icons
    
    * fix: natatoko aspect ratio
    
    * fix: resize icons
    
    * feat: project screenshots
    
    * fix: add img width and height to handle layout shift
    
    * feat: integration to medium and hashnode blog
    
    * feat: add links on the anchors

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+import { astroImageTools } from "astro-imagetools";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    astroImageTools,
+  ],
+  output: "server",
+  adapter: cloudflare(),
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        mini: "484px",
+        mob: "420px",
+      },
+      letterSpacing: {
+        tighter: "-0.04em",
+        tight: "-0.02em",
+        normal: "0",
+        wide: "0.05em",
+      },
+      lineHeight: {
+        100: "100%",
+        140: "140%",
+        150: "150%",
+      },
+      colors: {
+        dark: "#051C29",
+        cyan: {
+          dark: "#093853",
+        },
+        green: {
+          light: "#40F4B2",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
