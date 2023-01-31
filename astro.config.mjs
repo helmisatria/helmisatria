4c63c9a0685ef3d984f8166b4e5a592f07878c05
Helmi Satria <satriahelmi@gmail.com>
2023-01-31 14:49:00 +0700

    style: adjust styling blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,28 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import cloudflare from "@astrojs/cloudflare";
-import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://helmisatria.com",
-  integrations: [tailwind(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), astroImageTools, sitemap(), mdx({
-    shikiConfig: {
-      theme: "min-light"
-    }
-  }), image()],
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap(),
+    mdx({
+      shikiConfig: {
+        theme: "min-light",
+      },
+    }),
+    image(),
+  ],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
