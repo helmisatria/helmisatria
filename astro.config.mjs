897bb459baa185bed85a267a3ed2d93268a4c6ee
Helmi Satria <satriahelmi@gmail.com>
2023-01-31 10:14:31 +0700

    feat: slicing blog detailg

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,23 @@ import partytown from "@astrojs/partytown";
 import cloudflare from "@astrojs/cloudflare";
 import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://helmisatria.com",
-  integrations: [
-    tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    astroImageTools,
-    sitemap(),
-  ],
+  integrations: [tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), astroImageTools, sitemap(), mdx({
+    shikiConfig: {
+      theme: "min-light"
+    }
+  }), image()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
