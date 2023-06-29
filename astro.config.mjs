a5655e65aec7e973ab70dd8c7d3619c809b22038
Helmi Satria <satriahelmi@gmail.com>
2023-06-29 23:40:15 +0700

    fix: move to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,20 @@
-import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import image from "@astrojs/image";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx({
-      shikiConfig: {
-        theme: "github-dark",
-      },
-    }),
-    react(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [tailwind(), mdx({
+    shikiConfig: {
+      theme: "github-dark"
+    }
+  }), react(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   output: "server",
-  adapter: cloudflare(),
+  adapter: vercel()
 });
