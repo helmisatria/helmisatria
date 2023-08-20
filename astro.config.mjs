4020f4216c0a2412fb5a1f18bd8299c0361bff0d
Helmi Satria <satriahelmi@gmail.com>
2023-08-20 21:52:52 +0700

    fix: use experimental redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://helmisatria.com",
+  experimental: {
+    redirects: true,
+  },
   integrations: [
     tailwind(),
     mdx({
